923a8a34a8cf1030d2e60d214282367666d6d31e
eliot2903 <eliottesniere46@gmail.com>
2026-03-17 10:29:48 +0100

    Update app.py

--- projet/app.py
+++ projet/app.py
@@ -6,17 +6,17 @@ app = Flask(__name__)
 @app.route('/', methods=['GET', 'POST'])
 def home():
     message = ""
-    saisie = "" # On crée une variable vide au début
+    saisie = "" 
     cle = ""
     
     if request.method == 'POST':
-        # On récupère ce que l'utilisateur a tapé
+        
         saisie = request.form.get('phrase')
         cle=request.form.get("clé")
-        # On fait le traitement (ton programme)
+        
         message = test() 
         
-    # On renvoie les DEUX variables au HTML
+   
     return render_template('index.html', resultat=message, ancienne_phrase=saisie, ancienne_cle=cle)
 
 if __name__ == '__main__':
