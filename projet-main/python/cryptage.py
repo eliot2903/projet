d7190bbdaf46cbf1fa5b91cb6df40866e9ab101e
eliot2903 <eliottesniere46@gmail.com>
2026-04-04 18:00:36 +0200

    Add files via upload

---
+++ projet-main/python/cryptage.py
@@ -0,0 +1,111 @@
+import string
+import random
+
+def chiffre_de_vigenère(texte:str,cle:str,mode="cryptage"):
+    """
+    Cette fonction utilise le chiffre de vigenère pour crypter/décrypter une chaine de charactère.
+    Pour fonctionner la fonction a besoin d'un texte et d'une clé toute les deux sous formes de chaines de caractères.
+    Le chiffre de vigenère utilise les indices des lettres dans l'alphabet et décalle notre texte en fonction de la clé:
+    Par exemple : texte="abc" ,cle="a" texte_final="bcd" tout le texte a été décallé de 1 car l'indice de "a" dans l'alphabet est 1.
+    """
+    if texte :
+        if cle :
+            alphabet_min=string.ascii_lowercase
+            alphabet_maj=string.ascii_uppercase
+            cle=cle.replace(" ","")
+            for i in cle:
+                if i not in alphabet_maj and i not in alphabet_min:
+                    print("test")
+                    return "la cle ne doit avoir que des lettres de l'alphabet"
+            mot_crypté=""
+            indice_a=None
+            indice_b=None
+            indice_cle=0
+            for i in range(len(texte)):
+                if texte[i] in alphabet_maj or texte[i] in alphabet_min:
+                    est_maj=False
+                    if texte[i] in alphabet_maj:
+                        est_maj=True
+                        indice_a=alphabet_maj.index(texte[i])
+                    else:
+                        indice_a=alphabet_min.index(texte[i])
+                    if cle[indice_cle]in alphabet_maj:
+                        indice_b=alphabet_maj.index(cle[indice_cle])
+                    else:
+                        indice_b=alphabet_min.index(cle[indice_cle])
+                    indice_cle=(indice_cle+1)%len(cle)
+                    if mode=="cryptage":
+                        if est_maj:
+                            mot_crypté+=alphabet_maj[(indice_a+indice_b)%26]
+                        else:
+                            mot_crypté+=alphabet_min[(indice_a+indice_b)%26]
+                    else:
+                        if est_maj:
+                            mot_crypté+=alphabet_maj[(indice_a-indice_b)%26]
+                        else:
+                            mot_crypté+=alphabet_min[(indice_a-indice_b)%26]
+                else:
+                    mot_crypté+=texte[i]
+            return mot_crypté
+    return "veuiller écrire un texte"
+
+def cryptage_en_hexa(mot,mode="cryptage"):
+    """
+    Cette fonction tranforme un texte en héxadécimal ou l'inverse
+    """
+    if mot:
+        if mode=="cryptage":
+            code=mot.encode('utf-8').hex()
+        else:
+            code=bytes.fromhex(mot).decode('utf-8')
+        return code
+    return "veuiller écrire un texte"
+
+
+def Chiffre_de_Vernam(texte:str,cle:str=None,mode:str="cryptage"):
+    """
+    Cette algorithme marche comme le chiffre de vigenère mais il utilise une clé unique.
+    On vérifie donc si la clé est bien unique grace au fichier cle.txt.
+    Si la clé existe on en trouve une autre sinon on l'écrit dans le fichier
+    """
+    if texte :
+        alphabet_min=string.ascii_lowercase
+        alphabet_maj=string.ascii_uppercase
+        if cle==None:
+            cle=""
+            texte_sans_espace=texte.replace(" ","")
+            while True:
+                indice=True
+                for i in range(len(texte_sans_espace)):
+                    cle+=alphabet_min[random.randint(0,25)]
+                with open("python/cle.txt", "r", encoding="utf8") as file:
+                    for line in file.readlines():
+                        line = line.replace("\n", "")
+                        if line==cle:
+                            indice=False
+            
+                if indice:
+                    with open("python/cle.txt", "a", encoding="utf8") as file:
+                        file.write(f"{cle}\n")
+                    break
+        code=chiffre_de_vigenère(texte,cle,mode)
+        if mode=="cryptage":
+            return code,cle
+        else:
+            return code
+    return "veuiller écrire un texte"
+
+
+
+def test_fonction ():
+    assert chiffre_de_vigenère("j'aime la nsi","testeststetse")=="c'eafi dt flm"
+    assert chiffre_de_vigenère("c'eafi dt flm","testeststetse","decrypte")=="j'aime la nsi"
+    assert cryptage_en_hexa("bonjour")=="626f6e6a6f7572"
+    assert cryptage_en_hexa("626f6e6a6f7572","décryptage")=="bonjour"
+    assert Chiffre_de_Vernam('jfsejxi', 'irfvvdr',"décryptage")=="bonjour"
+    assert Chiffre_de_Vernam("tmmmy uachx","mibbkymlwu","decrypter")=="hello world"
+    assert chiffre_de_vigenère("","")=="veuiller écrire un texte"
+    assert Chiffre_de_Vernam("")=="veuiller écrire un texte"
+    assert cryptage_en_hexa("")=="veuiller écrire un texte"
+    assert chiffre_de_vigenère("123","test")=="123"
+test_fonction
