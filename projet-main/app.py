3972e9ea41fb6158c74045e35ed8dcf294dc1e39
wisax <eliottesniere46@gmail.com>
2026-04-14 19:25:10 +0200

    Update app.py

--- projet-main/app.py
+++ projet-main/app.py
@@ -29,17 +29,22 @@ def vernam():
     message = ""
     saisie = "" 
     cle = ""
+    
     if request.method == 'POST':
         saisie = request.form.get("Entre_texte")
+
         if saisie:
             message = Chiffre_de_Vernam(saisie) 
             return render_template('Chiffre_de_Vernam.html', resultat=message[1], resultat2=message[0],ancienne_cle2=cle,ancien_texte2=saisie)
+        
         else:
             saisie = request.form.get("Entre_texte2")
             cle = request.form.get("Cle2")
+
             if saisie and cle:
                 message=Chiffre_de_Vernam(saisie,cle,"décryptage")
                 return render_template('Chiffre_de_Vernam.html', resultat3=message)
+            
     return render_template('Chiffre_de_Vernam.html')
 
 @app.route('/vigenere', methods=['GET', 'POST'])
@@ -47,20 +52,23 @@ def vigenere():
     message = ""
     saisie = "" 
     cle = ""
+
     if request.method == 'POST':
         saisie = request.form.get("Entre_texte")
         cle=request.form.get("Cle")
+
         if saisie and cle:
             message = chiffre_de_vigenère(saisie,cle) 
             return render_template('Chiffre_de_Vigenère.html', resultat=message,ancienne_cle=cle,ancien_texte=saisie)
+        
         else:
             saisie = request.form.get("Entre_texte2")
             cle = request.form.get("Cle2")
-            print(saisie,cle)
+
             if saisie and cle:
                 message=chiffre_de_vigenère(saisie,cle,"décryptage")
-                print(message)
                 return render_template('Chiffre_de_Vigenère.html', resultat2=message,ancienne_cle2=cle,ancien_texte2=saisie)
+            
     return render_template('Chiffre_de_Vigenère.html')
 
 @app.route('/hexa', methods=['GET', 'POST'])
@@ -68,13 +76,16 @@ def hexa():
     saisie=""
     if request.method == 'POST':
         saisie=request.form.get("Entre_texte")
+
         if saisie:
             message=cryptage_en_hexa(saisie)
             return render_template('Hexadecimal.html',resultat2=message)
+        
         else:
             saisie=request.form.get("Entre_texte2")
             message=cryptage_en_hexa(saisie,"decryptage")
             return render_template('Hexadecimal.html',resultat3=message)
+        
     return render_template('Hexadecimal.html')
 
 
