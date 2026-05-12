4f37fe6ce5e314fa0541d4e0adf57feb99301812
wisax <eliottesniere46@gmail.com>
2026-05-12 18:53:34 +0200

    Update app.py

--- projet-main/app.py
+++ projet-main/app.py
@@ -14,6 +14,11 @@ app = Flask(__name__)
 def home():
     return render_template("Page_d'accueil.html")
 
+@app.route('/google')
+def google():
+    return render_template("googleb8007cc217fdedbc.html")
+
+
 @app.route('/desc_hexa')
 def desc_hexa():
     return render_template("Description_Hexadecimal.html")
